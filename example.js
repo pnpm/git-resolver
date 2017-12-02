6caf2af79faae7c44b85ee8e6409abb13e236db1
Zoltan Kochan <z@kochan.io>
2017-12-02 20:52:39 +0200

    docs: fix example

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ const resolveFromNpm = createResolveFromNpm({getJson})
 resolveFromNpm({
   pref: 'kevva/is-negative#16fd36fe96106175d02d066171c44e2ff83bc055'
 }, { getJson })
-.then(resolveResult => console.log(resolveResult))
+.then(resolveResult => console.log(JSON.stringify(resolveResult, null, 2)))
 
 function getJson (url, registry) {
   return got(url, {json: true})
