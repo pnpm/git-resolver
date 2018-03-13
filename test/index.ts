6f8c51d41831243a3650a53a627dd5bf8e94a7d3
Zoltan Kochan <z@kochan.io>
2018-03-13 22:46:10 +0200

    test: use zkochan/is-negative repo for tests

--- test/index.ts
+++ test/index.ts
@@ -4,36 +4,36 @@ import createResolveFromGit from '@pnpm/git-resolver'
 const resolveFromGit = createResolveFromGit({})
 
 test('resolveFromGit()', async t => {
-  const resolveResult = await resolveFromGit({pref: 'kevva/is-negative#163360a8d3ae6bee9524541043197ff356f8ed99'})
+  const resolveResult = await resolveFromGit({pref: 'zkochan/is-negative#163360a8d3ae6bee9524541043197ff356f8ed99'})
   t.deepEqual(resolveResult, {
-    id: 'github.com/kevva/is-negative/163360a8d3ae6bee9524541043197ff356f8ed99',
-    normalizedPref: 'github:kevva/is-negative#163360a8d3ae6bee9524541043197ff356f8ed99',
+    id: 'github.com/zkochan/is-negative/163360a8d3ae6bee9524541043197ff356f8ed99',
+    normalizedPref: 'github:zkochan/is-negative#163360a8d3ae6bee9524541043197ff356f8ed99',
     resolution: {
-      tarball: 'https://codeload.github.com/kevva/is-negative/tar.gz/163360a8d3ae6bee9524541043197ff356f8ed99'
+      tarball: 'https://codeload.github.com/zkochan/is-negative/tar.gz/163360a8d3ae6bee9524541043197ff356f8ed99'
     }
   })
   t.end()
 })
 
 test('resolveFromGit() with strict semver', async t => {
-  const resolveResult = await resolveFromGit({pref: 'kevva/is-negative#semver:1.0.0'})
+  const resolveResult = await resolveFromGit({pref: 'zkochan/is-negative#semver:1.0.0'})
   t.deepEqual(resolveResult, {
-    id: 'github.com/kevva/is-negative/163360a8d3ae6bee9524541043197ff356f8ed99',
-    normalizedPref: 'github:kevva/is-negative#semver:1.0.0',
+    id: 'github.com/zkochan/is-negative/163360a8d3ae6bee9524541043197ff356f8ed99',
+    normalizedPref: 'github:zkochan/is-negative#semver:1.0.0',
     resolution: {
-      tarball: 'https://codeload.github.com/kevva/is-negative/tar.gz/163360a8d3ae6bee9524541043197ff356f8ed99'
+      tarball: 'https://codeload.github.com/zkochan/is-negative/tar.gz/163360a8d3ae6bee9524541043197ff356f8ed99'
     }
   })
   t.end()
 })
 
 test('resolveFromGit() with range semver', async t => {
-  const resolveResult = await resolveFromGit({pref: 'kevva/is-negative#semver:^1.0.0'})
+  const resolveResult = await resolveFromGit({pref: 'zkochan/is-negative#semver:^1.0.0'})
   t.deepEqual(resolveResult, {
-    id: 'github.com/kevva/is-negative/9a89df745b2ec20ae7445d3d9853ceaeef5b0b72',
-    normalizedPref: 'github:kevva/is-negative#semver:^1.0.0',
+    id: 'github.com/zkochan/is-negative/9a89df745b2ec20ae7445d3d9853ceaeef5b0b72',
+    normalizedPref: 'github:zkochan/is-negative#semver:^1.0.0',
     resolution: {
-      tarball: 'https://codeload.github.com/kevva/is-negative/tar.gz/9a89df745b2ec20ae7445d3d9853ceaeef5b0b72'
+      tarball: 'https://codeload.github.com/zkochan/is-negative/tar.gz/9a89df745b2ec20ae7445d3d9853ceaeef5b0b72'
     }
   })
   t.end()
